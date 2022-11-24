b2f9e6177dbb21d68c986a0470ed518413d8d666
Chetyrkin Alex <pyroponya@gmail.com>
2022-11-24 16:16:36 +0300

    wqe

--- api/index.js
+++ api/index.js
@@ -58,4 +58,33 @@ await new Promise((resolve, reject) => {
 res.status(200).json({ status: "OK" });
 };
 
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+app.post('/email', (req, res) => {
+  const { from, to, subject, text } = req.body;
+  const mailData = {
+    from: from,
+    to: to,
+    subject: subject,
+    text: text,
+  };
+
+  transporter.sendMail(mailData, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    res.status(200).send({message: 'Mail send'});
+  })
+})
+
 module.exports = app;
