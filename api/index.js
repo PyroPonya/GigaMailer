678f9b1780cddd48a1a7a567d63c57b011d6085b
Chetyrkin Alex <pyroponya@gmail.com>
2022-11-24 17:38:42 +0300

    eqw

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
+
+// app.post('/api', (req, res) => {
+//   const { from, to, subject, text } = req.body;
+//   const mailData = {
+//     from: from,
+//     to: to,
+//     subject: subject,
+//     text: text,
+//   };
+
+//   transporter.sendMail(mailData, (error, info) => {
+//     if (error) {
+//       return console.log(error);
+//     }
+//     res.status(200).send({message: 'Mail send'});
+//   })
+// })
