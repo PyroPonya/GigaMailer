ad837eb8bbeae93c0432c7c789ddc0cd5f185108
Chetyrkin Alex <pyroponya@gmail.com>
2022-11-24 16:31:36 +0300

    weq

--- api/index.js
+++ api/index.js
@@ -1,63 +1,6 @@
 const app = require('express')();
 const { v4 } = require('uuid');
 
-const baseMail = 'info@kosmospromedia.ru';
-
-export default async (req, res) => {
-
-const { firstName, lastName, email, message } = JSON.parse(req.body);
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-      user: "myEmail@gmail.com",
-      pass: "password",
-  },
-});
-
-await new Promise((resolve, reject) => {
-    // verify connection configuration
-    transporter.verify(function (error, success) {
-        if (error) {
-            console.log(error);
-            reject(error);
-        } else {
-            console.log("Server is ready to take our messages");
-            resolve(success);
-        }
-    });
-});
-
-const mailData = {
-    from: {
-        name: `${firstName} ${lastName}`,
-        address: "myEmail@gmail.com",
-    },
-    replyTo: email,
-    to: "recipient@gmail.com",
-    subject: `form message`,
-    text: message,
-    html: `${message}`,
-};
-
-await new Promise((resolve, reject) => {
-    // send mail
-    transporter.sendMail(mailData, (err, info) => {
-        if (err) {
-            console.error(err);
-            reject(err);
-        } else {
-            console.log(info);
-            resolve(info);
-        }
-    });
-});
-
-res.status(200).json({ status: "OK" });
-};
-
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
@@ -70,7 +13,7 @@ app.get('/api/item/:slug', (req, res) => {
   res.end(`Item: ${slug}`);
 });
 
-app.post('/email', (req, res) => {
+app.post('/api', (req, res) => {
   const { from, to, subject, text } = req.body;
   const mailData = {
     from: from,
---
+++ api/send-email.js
@@ -0,0 +1,56 @@
+import nodemailer from 'nodemailer';
+
+export default async (req, res) => {
+  
+  const { firstName, lastName, email, message } = JSON.parse(req.body);
+  const baseMail = 'info@kosmospromedia.ru';
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "myEmail@gmail.com",
+        pass: "password",
+    },
+  });
+
+  await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transporter.verify(function (error, success) {
+          if (error) {
+              console.log(error);
+              reject(error);
+          } else {
+              console.log("Server is ready to take our messages");
+              resolve(success);
+          }
+      });
+  });
+
+  const mailData = {
+      from: {
+          name: `${firstName} ${lastName}`,
+          address: "myEmail@gmail.com",
+      },
+      replyTo: email,
+      to: "recipient@gmail.com",
+      subject: `form message`,
+      text: message,
+      html: `${message}`,
+  };
+
+  await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailData, (err, info) => {
+          if (err) {
+              console.error(err);
+              reject(err);
+          } else {
+              console.log(info);
+              resolve(info);
+          }
+      });
+  });
+  res.status(200).json({ status: "OK" });
+};
