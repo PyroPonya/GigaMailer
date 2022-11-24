49c2ab7c97a7df0521e8228ab1fb62fdb3a55330
Chetyrkin Alex <pyroponya@gmail.com>
2022-11-24 16:10:02 +0300

    qwe

--- api/index.js
+++ api/index.js
@@ -58,16 +58,4 @@ await new Promise((resolve, reject) => {
 res.status(200).json({ status: "OK" });
 };
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
 module.exports = app;
