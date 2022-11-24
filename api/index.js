ecd5223738b59f1f4296e2912c1543fc9f8b04cc
Chetyrkin Alex <pyroponya@gmail.com>
2022-11-24 17:15:24 +0300

    ewq

--- api/index.js
+++ api/index.js
@@ -6,33 +6,33 @@ export default async function handler(req, res) {
   return res.send(`Hello ${body.name}, you just parsed the request body!`);
 }
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+// app.get('/api', (req, res) => {
+//   const path = `/api/item/${v4()}`;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+//   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+// });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
+// app.get('/api/item/:slug', (req, res) => {
+//   const { slug } = req.params;
+//   res.end(`Item: ${slug}`);
+// });
 
-app.post('/api', (req, res) => {
-  const { from, to, subject, text } = req.body;
-  const mailData = {
-    from: from,
-    to: to,
-    subject: subject,
-    text: text,
-  };
+// app.post('/api', (req, res) => {
+//   const { from, to, subject, text } = req.body;
+//   const mailData = {
+//     from: from,
+//     to: to,
+//     subject: subject,
+//     text: text,
+//   };
 
-  transporter.sendMail(mailData, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    res.status(200).send({message: 'Mail send'});
-  })
-})
+//   transporter.sendMail(mailData, (error, info) => {
+//     if (error) {
+//       return console.log(error);
+//     }
+//     res.status(200).send({message: 'Mail send'});
+//   })
+// })
 
 module.exports = app;
