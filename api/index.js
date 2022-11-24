61a8cd267d592031bf7794563abfa1f8f638138f
Chetyrkin Alex <pyroponya@gmail.com>
2022-11-24 17:20:08 +0300

    df

--- api/index.js
+++ api/index.js
@@ -1,22 +1,19 @@
-// const app = require('express')();
-// const { v4 } = require('uuid');
+const app = require('express')();
+const { v4 } = require('uuid');
 
-export default async function handler(req, res) {
-  const { body } = req;
-  return res.send(`Hello ${body.name}, you just parsed the request body!`);
-}
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
-// app.get('/api', (req, res) => {
-//   const path = `/api/item/${v4()}`;
-//   res.setHeader('Content-Type', 'text/html');
-//   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-//   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-// });
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
-// app.get('/api/item/:slug', (req, res) => {
-//   const { slug } = req.params;
-//   res.end(`Item: ${slug}`);
-// });
+module.exports = app;
 
 // app.post('/api', (req, res) => {
 //   const { from, to, subject, text } = req.body;
@@ -34,5 +31,3 @@ export default async function handler(req, res) {
 //     res.status(200).send({message: 'Mail send'});
 //   })
 // })
-
-// module.exports = app;
